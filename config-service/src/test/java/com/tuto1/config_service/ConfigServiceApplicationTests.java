a543d326eee90d576b743b8bd71dd152cc36891d
SamDpg <48963680+SamDpg@users.noreply.github.com>
2024-09-25 20:04:47 +0100

    first commit
    maybe this commit allow me the program start working

---
+++ config-service/src/test/java/com/tuto1/config_service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuto1.config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
