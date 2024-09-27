5ccc419489e44d4a3c5f4e460b4501a1b51322b4
SamDpg <48963680+SamDpg@users.noreply.github.com>
2024-09-27 20:29:43 +0100

    first commit
    first try to make work the app

--- bikeservice/src/main/java/com/tuto1/bikeservice/BikeserviceApplication.java
+++ bikeservice/src/main/java/com/tuto1/bikeservice/BikeserviceApplication.java
@@ -2,8 +2,10 @@ package com.tuto1.bikeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BikeserviceApplication {
 
 	public static void main(String[] args) {
